b7c1684754522986dee521749a821c8fdde892ab
Alfredodaniello <alfredo_daniello@libero.it>
2024-06-17 17:21:45 +0200

    return projects from api

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -4,10 +4,15 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Project;
 
 class ProjectController extends Controller
 {
     public function index(){
-        dd('ciao');
+        $projects = Project::all();
+        return response()->json([
+            'succes'=> true,
+            'results'=> $projects,
+        ]);
     }
 }
